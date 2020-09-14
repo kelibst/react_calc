e2871bfc18acd378143fef9673f3d3700bad633c
kelibst <kboster17@gmail.com>
2020-09-14 09:51:21 +0000

    added input space

--- src/components/DisplayPanel.js
+++ src/components/DisplayPanel.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
-export default function DisplayPanel() {
+import PropTypes from 'prop-types'
+function DisplayPanel(props) {
+    const { res } = props;
     return (
         <div className="disContainer">
-           0 
+           <input type="text" value={res}/>
         </div>
     )
 }
+
+//the default value
+DisplayPanel.defaultProps = {
+    res: '0'
+}
+//the result from the props should be a string
+DisplayPanel.propTypes = {
+    res: PropTypes.string
+}
+
+export default  DisplayPanel
