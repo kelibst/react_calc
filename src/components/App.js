8b4f110f018588b9c34e8a7b73824ba7d4a6494d
kelibst <kboster17@gmail.com>
2020-09-16 10:16:14 +0000

    fixed linters

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import calculate from '../logic/calculate';
 import ButtonCov from './ButtonCov';
 import DisplayPanel from './DisplayPanel';
-
 
 function App() {
   return (
